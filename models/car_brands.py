7a7c38b88752c3085596429233116a2169e122fb
Davi <daviaugustos@gmail.com>
2026-04-05 19:39:38 -0300

    Clean up duplicate files at root level - monorepo structure only

--- models/car_brands.py
+++
@@ -1,7 +0,0 @@
-from sqlalchemy import Column, Integer, String
-from database.connection import Base
-
-class Car_brands(Base):
-    __tablename__ = "car_brands"
-    brand_id = Column(Integer, primary_key=True, index=True, nullable=False)
-    brand_name = Column(String(255), nullable=True)
